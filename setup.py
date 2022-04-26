0ab48006c7d2e80b93d24b176fd82ca66f7be18c
Max Lange <M.T.S.Lange@student.tudelft.nl>
2022-04-26 16:40:26 +0200

    Add requirements, setup.py and proposal document

---
+++ setup.py
@@ -0,0 +1,16 @@
+"""
+ Package information
+"""
+
+from setuptools import setup
+
+
+setup(name='APDUDS',
+      version='0.0',
+      description='Automated Preliminary Design of Urban Drainage Systems',
+      author='Max Lange',
+      author_email='langemax0@gmail.com',
+      url='https://github.com/Max-Lange/APDUDS',
+      packages=['APDUDS'],
+      license='GNU General Public License',
+      )
